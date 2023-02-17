8c2060b242bc692c66ad75bf6a38139adec4d693
Dmitry Plotnikov <103034855+oOBUGagaOo@users.noreply.github.com>
2023-02-17 10:08:36 +0300

    added getMessage() method

--- src/main/java/blow/back/Main.java
+++ src/main/java/blow/back/Main.java
@@ -2,7 +2,10 @@ package blow.back;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Привет qaGURU!");
+        System.out.println(getMessage());
     }
+    private static String getMessage(){
+        return "Hallo GURU";
+    }
+
 }
