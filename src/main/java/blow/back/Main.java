1f468c232736fed1e44e3fada079d3f819cad89a
Dmitry Plotnikov <103034855+oOBUGagaOo@users.noreply.github.com>
2023-02-17 10:48:34 +0300

    added Hello message

--- src/main/java/blow/back/Main.java
+++ src/main/java/blow/back/Main.java
@@ -1,10 +1,10 @@
 package blow.back;
 
 public class Main {
-    private static String a = ">>Hi qaGURU";
+    private static String b = "Привет qaGURU2";
 
     public static void main(String[] args) {
 
-        System.out.println("a");
+        System.out.println(b);
     }
 }
