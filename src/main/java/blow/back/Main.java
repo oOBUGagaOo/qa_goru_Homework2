50b04aefc94e59f0916362ae053274b30c096870
Dmitry Plotnikov <103034855+oOBUGagaOo@users.noreply.github.com>
2023-02-17 10:42:59 +0300

    Update Main.java

--- src/main/java/blow/back/Main.java
+++ src/main/java/blow/back/Main.java
@@ -1,10 +1,9 @@
 package blow.back;
 
 public class Main {
-    private static String a = ">>Hi qaGURU";
 
     public static void main(String[] args) {
 
-        System.out.println("a");
+        System.out.println(">>Hi qaGURU");
     }
 }
